8532bae74cb85d7b4969bb1b389e35ea8732c207
Slyadnev Sergey Aleksandrovich <42943522+SergeiSd@users.noreply.github.com>
2021-07-18 14:06:17 +0300

    fix errors
    added typing module for functions
    added raise SystemExit in function "create_connection"

--- Project_3/create_db.py
+++ Project_3/create_db.py
@@ -3,6 +3,8 @@ import psycopg2
 
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2 import Error
+from typing import Optional, Union
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--db_name', type=str, default='sensor_data',
@@ -18,8 +20,8 @@ parser.add_argument('--port', type=str, default='5432',
 args = parser.parse_args()
 
 
-def create_connection(db_user, db_password,
-                      db_host, db_port, db_name=None):
+def create_connection(db_user: str, db_password: str, db_host: str,
+                      db_port: str, db_name: Optional[str] = None):
     """A function to create a connection to a PostgreSQL database instance.
 
     Parameters:
@@ -51,7 +53,11 @@ def create_connection(db_user, db_password,
         A connection object to a PostgreSQL database instance.
     """
 
-    print('Connection to PostgreSQL...')
+    if db_name is None:
+        print('Connection to PostgreSQL...')
+    else:
+        print('Connection to PostgreSQL Database...')
+
     connection = None
     try:
         connection = psycopg2.connect(
@@ -61,14 +67,19 @@ def create_connection(db_user, db_password,
             port=db_port,
             database=db_name
         )
-        print("Connection to PostgreSQL successful.")
+        if db_name is None:
+            print("Connection to PostgreSQL successful.")
+        else:
+            print('Connection to PostgreSQL Database successful.')
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+        raise SystemExit
+    else:
+        return connection
 
-    return connection
 
-
-def create_database(connection: psycopg2.extensions.connection) -> None:
+def create_database(connection:
+                    psycopg2.extensions.connection) -> Union[bool, None]:
     """Database creation in PostgreSQL.
 
     Database for data from the sensor.
@@ -127,6 +138,7 @@ def create_table(connection: psycopg2.extensions.connection) -> None:
                          HUMIDITY REAL NOT NULL,
                          TIMESTAMP TIMESTAMP WITHOUT TIME ZONE NOT NULL);
                          '''
+
     print('Creating a table...')
     try:
         cursor.execute(create_table_query)
